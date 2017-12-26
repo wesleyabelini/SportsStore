8ee6816908f29c92a9bc8b4bb272c0ea41ea01d7
Wesley Abelini <w_vicente.abelini@hotmail.com>
2017-12-26 17:58:33 -0200

    data

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,23 @@ namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [Key]
         public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(500)]
         public string Description { get; set; }
-        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Category { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+        
     }
 }

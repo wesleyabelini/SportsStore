756baff647265fe4b46115dee9cce1dc1482e345
Wesley Abelini <w_vicente.abelini@hotmail.com>
2017-12-26 17:03:00 -0200

    Initial

---
+++ SportsStore.Web.UI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.Web.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+
+        public ProductController(IProductsRepository productsRepository)
+        {
+            this.repository = productsRepository;
+        }
+
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ SportsStore.Web.UI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.Web.UI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product{Name="Football", Price=25},
+                new Product{Name="Surf board", Price=179},
+                new Product{Name="Running shoes", Price=95}
+            });
+
+            kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
